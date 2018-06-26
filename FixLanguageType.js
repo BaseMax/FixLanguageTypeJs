27a915a768b927108c46c24d7d9ae96bd49a61f3
Max Base <MaxBaseCode@Gmail.Com>
2018-06-26 21:49:59 +0430

    Update FixLanguageType.js

--- FixLanguageType.js
+++ FixLanguageType.js
@@ -1,1 +1,10 @@
-
+/**
+*
+* @Name : FixLanguageType.js
+* @Version : 1.0
+* @Programmer : Max
+* @Date : 2018-06-26
+* @Released under : https://github.com/BaseMax/FixLanguageTypeJs/blob/master/LICENSE
+* @Repository : https://github.com/BaseMax/FixLanguageTypeJs
+*
+**/
