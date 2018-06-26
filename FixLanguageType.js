f02f9c88f29f88673803bd30b42454eb1d8edcff
Max Base <MaxBaseCode@Gmail.Com>
2018-06-26 22:13:08 +0430

    Update FixLanguageType.js

--- FixLanguageType.js
+++ FixLanguageType.js
@@ -8,3 +8,136 @@
 * @Repository : https://github.com/BaseMax/FixLanguageTypeJs
 *
 **/
+;(function(window,document)
+{
+	"use strict";
+	/**
+	* @function keymap
+	*
+	* @goal : Keymap of en , fa
+	*
+	* @return array (key=en , value=fa)
+	**/
+	var keymap=
+	{
+		'q':"ض",
+		'w':"ص",
+		'e':"ث",
+		'r':"ق",
+		't':"ف",
+		'y':"غ",
+		'u':"ع",
+		'i':"ه",
+		'o':"خ",
+		'p':"ح",
+		'[':"ج",
+		']':"چ",
+		'a':"ش",
+		's':"س",
+		'd':"ی",
+		'f':"ب",
+		'g':"ل",
+		'h':"ا",
+		'j':"ت",
+		'k':"ن",
+		'l':"م",
+		';':"ک",
+		'\'':"گ",
+		'z':"ظ",
+		'x':"ط",
+		'c':"ز",
+		'v':"ر",
+		'b':"ذ",
+		'n':"د",
+		'm':"پ",
+		',':"و",
+	};
+	/**
+	* @function replaceAll
+	*
+	* @goal : Replace All
+	*
+	* @return string
+	**/
+	var replaceAll = function(input,str1, str2, ignore)
+	{
+		return input.replace(new RegExp(str1.replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\<\>\-\&])/g,"\\$&"),(ignore?"gi":"g")),(typeof(str2)=="string")?str2.replace(/\$/g,"$$$$"):str2);
+	}
+	/**
+	* @function en_fa
+	*
+	* @goal : Change `en` character to `fa` character in self keymap!
+	*
+	* @return void
+	**/
+	var en_fa=function(value)
+	{
+		var keys=Object.keys(keymap);
+		var values=Object.values(keymap);
+		for(var i=0;i<keys.length;i++)
+			value=replaceAll(value,keys[i],values[i]);
+		return value;
+	};
+	/**
+	* @function fa_en
+	*
+	* @goal : Change `fa` character to `en` character in self keymap!
+	*
+	* @return void
+	**/
+	var fa_en=function(value)
+	{
+		var keys=Object.values(keymap);
+		var values=Object.keys(keymap);
+		for(var i=0;i<keys.length;i++)
+			value=replaceAll(value,keys[i],values[i]);
+		return value;
+	};
+	/**
+	* @struct fixtype
+	*
+	* @goal : access to public functions
+	*
+	* @return struct
+	**/
+	window.fixtype=
+	{
+		en_fa:en_fa,
+		fa_en:fa_en,
+	};
+	/**
+	* @struct onload
+	*
+	* @goal : set onclick and events after page load...
+	*
+	* @return void
+	**/
+	window.addEventListener("load",function()
+	{
+		var data_goscroll;
+		data_goscroll = document.querySelectorAll("[data-scroll-go]");
+		data_goscroll.forEach(function(item)
+		{
+			if(item.onclick === null)//onclick not exists
+			{
+				item.onclick=function()
+				{
+					window.goscroll.go(this);
+				};
+			}
+		});
+		//////////////////////////////////////////////////////////////////
+		data_goscroll = document.querySelectorAll("[data-scroll-to]");
+		data_goscroll.forEach(function(item)
+		{
+			if(item.onclick === null)//onclick not exists
+			{
+				item.onclick=function()
+				{
+					window.goscroll.goto(this);
+				};
+			}
+		});
+		//////////////////////////////////////////////////////////////////
+	},false);
+}(window,document));
